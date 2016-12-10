fc522fc4721ef85ef0b41704f2849644c3aaa49a
jonabasque <jonmultimedia@gmail.com>
2016-12-10 21:47:05 +0100

    Created starter data & got parliament for collection in viewParliament component

--- imports/ui/components/viewParliament/viewParliament.js
+++ imports/ui/components/viewParliament/viewParliament.js
@@ -1,15 +1,25 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
+//TODO: Recibir por get o post el objeto con el 'parli' en lugar de hacer otra consulta a la BBDD
+import { Parliaments } from '../../../api/parliaments';
+
 import template from './viewParliament.html';
 const name = 'viewParliament';
 
 class ViewParliament {
   constructor($stateParams, $scope, $reactive) {
     'ngInject';
+    //var parlis = Parliaments.findOne({name: $stateParams.parliamentName });
+    //console.log(parlis);
+    //this.par = parlis.parliamentName;
+    $reactive(this).attach($scope);
 
-    this.name = $stateParams.parliamentName;
-
+    this.helpers({
+      parli() {
+        return Parliaments.findOne({name: $stateParams.parliamentName });
+      }
+    });
   }
 }
 
