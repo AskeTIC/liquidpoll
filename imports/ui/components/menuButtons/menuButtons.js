9db8cdfbdf2a0c99c0233a6fd029fc10c12dc5c6
jonabasque <jonmultimedia@gmail.com>
2017-01-20 20:36:07 +0100

    Created 'menuButtons' component & used in 'viewAgora' & 'viewAgoraList' components

---
+++ imports/ui/components/menuButtons/menuButtons.js
@@ -0,0 +1,30 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import template from './menuButtons.html';
+
+const name = 'menuButtons';
+
+class MenuButtons {
+  constructor($stateParams, $scope, $reactive) {
+    'ngInject';
+    $reactive(this).attach($scope);
+    console.log('menuButtons Controller !!!!!!!!!!!!!');
+    console.log(this.items);
+  }
+
+}
+
+// create a module with a componente
+export default angular.module( name, [
+  angularMeteor ])
+    .component(name, {
+        transclude: true,
+        bindings: {
+            items: '<',
+            classes: '@'
+        },
+        templateUrl: template,
+        controller: MenuButtons,
+    }
+);
