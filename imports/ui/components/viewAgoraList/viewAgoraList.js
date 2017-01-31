de0010cfe509a19917ddc13dcf968324556f8f62
jonabasque <jonmultimedia@gmail.com>
2017-01-31 22:14:37 +0100

    Removed provisional button & subscription in 'viewAgoraList' component

--- imports/ui/components/viewAgoraList/viewAgoraList.js
+++ imports/ui/components/viewAgoraList/viewAgoraList.js
@@ -2,13 +2,7 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
 import { Agoras } from '../../../api/agoras/agoras'; //this cursors is going to work with monimongo
-import { Meteor } from 'meteor/meteor';
 
-Meteor.subscribe('agoras', {
-    onReady: function(){
-        console.log("Subscrito a las "+ Agoras.find({}).count()+ " agoras!!");
-    }
-})
 //Componentes incluidos (dependencias)
 import { name as chartsBars } from '../chartsBars/chartsBars';
 //import { name as chartsPie } from '../chartsPie/chartsPie';
@@ -20,6 +14,9 @@ class ViewAgoraList {
   constructor($stateParams, $scope, $reactive) {
     'ngInject';
     $reactive(this).attach($scope);
+
+    //TODO: Emitir este evento cuando cambia los datos de la susbcripción.
+    //$scope.$broadcast('change-entities'); //Esto es para un solo cliente, no hay sincronización.
 
     var options = {
         bar : {
@@ -33,47 +30,13 @@ class ViewAgoraList {
         }
     }
 
-    var agora = { "name" : "Guatemala",
-                  "slug" : "guatemala",
-                  "description" : "Ágora guatemalteca.",
-                  "entities" : [
-                      { "name" : "Cambiemos", "siglas" : "Cambiemos", "points" : 5, "percent" : null, "color" : "blue" },
-                      { "name" : "Frente para la victoria","siglas" : "FxV","points" : 2,"percent" : null, "color" : "pink" },
-                      { "name" : "Justicia social", "siglas" : "JS", "points" : 3, "percent" : null, "color" : "brown" },
-                      { "name" : "Todo por la plata", "siglas" : "TxP", "points" : 10, "percent" : null, "color" : "lightgreen" },
-                      { "name" : "Iluminatis argentinos", "siglas" : "Iluminatis", "points" : 12, "percent" : null, "color" : "yellow" },
-                      { "name" : "Partido maligno", "siglas" : "PM", "points" : 13, "percent" : null, "color" : "orange" },
-                      { "name" : "Agarralo como puedas", "siglas" : "Agarralo", "points" : 7, "percent" : null, "color" : "violet" },
-                      { "name" : "Los viejos verdes", "siglas" : "LV", "points" : 4, "percent" : null, "color" : "green" },
-                      { "name" : "Los jovenes con el pindar rojo", "siglas" : "PC", "points" : 4, "percent" : null, "color" : "red" },
-                      { "name" : "Los hombres de negro", "siglas" : "Sociatas", "points" : 1, "percent" : null, "color" : "lightblue" }
-                  ],
-                  "subagoras" : [
-                      { "name" : "Cordoba", "slug" : "cordoba" },
-                      { "name" : "Buenos Aires", "slug" : "buenos-aires" }
-                  ],
-                  "topagoras" : [ ],
-                  "settings" : { "nodes" : [ 1 ], "area-level" : 4, "area-type" : 3 }
-              };
-
-
-
-    //TOKNOW: Los helpers se ejecutan cunado se crea el controller, para poner datos o métodos en disposicion de la vista, sin embargo si
     this.helpers({
         options() {
             return options;
         },
-        //TODO: Si cambia un valor debe de cambiar el tiempo real.
         agoras() {
-            //TODO: Extraer solo las que pueda ver el user, y ordenar de level area más grande a menos.
+            //TODO: Extraer solo las que pueda ver el user, y ordenar de level area más grande a menos (por decidir).
             return Agoras.find({});
-        },
-        addAgora() {
-            console.log('preparando helper addAgora');
-            return function(){
-                console.log('AddAgora!!!!');
-                Agoras.insert(agora);
-            }
         }
     });
   }
