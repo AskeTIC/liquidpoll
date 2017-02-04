5e4737fa5bdb4dcd024170f9874d58cbdf9f6d08
jonabasque <jonmultimedia@gmail.com>
2017-02-04 20:35:06 +0100

    Added a new publication for entities

--- imports/api/agoras/server/publications.js
+++ imports/api/agoras/server/publications.js
@@ -5,6 +5,10 @@ export default function(){
     Meteor.publish('agoras', function(){
         //TODO: Publicar solo las que tenga acceso el userId o todas y limitar la acción de votar (por decidir).
         //En la segunda opción, habría que poner una paginación y buscador, o un mapa del mundo.
-        return Agoras.find({});
+        return Agoras.find({}, {fields: {entities: 0}});
+    });
+    Meteor.publish('entities', function(agoraSlug){
+        //TODO: validar agoraSlug
+        return Agoras.find({'slug': agoraSlug}, {fields: {entities: 1}});
     });
 }
