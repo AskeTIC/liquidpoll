f62fac6240599d3cad2d5415457583943dd7653f
jonabasque <jonmultimedia@gmail.com>
2017-02-16 19:51:04 +0100

    Captured 'onLogin()' for redirect to user dashboard in first login & increment 'loggings' profile attr

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Accounts } from 'meteor/accounts-base';
 //import angularElastic from 'angular-elastic';
 
 import { name as webMain } from '../imports/ui/components/webMain/webMain';
@@ -22,6 +23,34 @@ app.run(runWebapp);
 
 function configWebapp(){
     console.log('config() de webapp.......');
+    //var userLoggings = Meteor.subscribe("userLoggings").fetch();
+    //console.log(userLoggings);
+    Accounts.onLogin(function(){
+        console.log("onLogin() en cliente!!!");
+        var user = Meteor.user();
+        console.log(user);
+        if(user.profile.loggings === 0){
+            console.log("Es el primer logging, así que le redirijo a su dashboard");
+            //TODO: redirigir al dashboard de usuario.
+
+        }
+        //TODO: sumar +1 a profile.logging
+        user.profile.loggings += 1;
+        console.log(Meteor.userId());
+        Meteor.users.update({ "_id": Meteor.userId()}, { $set : {"profile": user.profile}});
+    });
+
+    /*
+    Tracker.autorun(function(){
+        console.log("Tracker.autorun() dependiente de Accounts.loggingIn()");
+        //Si vengo de no haber usuario logueado, por que si cambia el valor de Accounts.loggingIn()
+        //de false a true o de true a false se ejecutará el autorun() 2 veces muy rápidas.
+        if(!Meteor.userId){
+            //Quizás usar onLogin() que es cuando se ha producido el login.
+            Accounts.loggingIn();
+        }
+    });
+    */
 }
 
 function runWebapp($rootScope){
