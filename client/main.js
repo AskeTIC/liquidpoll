a04f3319249c32c6d574731322f55e76b8f6e610
jonabasque <jonmultimedia@gmail.com>
2017-02-20 01:05:40 +0100

    Moved routing to webapp config() & incluyed dashboard with params for default

--- client/main.js
+++ client/main.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { Accounts } from 'meteor/accounts-base';
+import uiRouter from 'angular-ui-router';
+import { name as WebAppServices } from './js/services/webAppServices';
 //import angularElastic from 'angular-elastic';
 
 import { name as webMain } from '../imports/ui/components/webMain/webMain';
@@ -10,41 +12,48 @@ import { name as webHeader } from '../imports/ui/components/webHeader/webHeader'
 import { name as webFooter } from '../imports/ui/components/webFooter/webFooter';
 
 var app = angular.module('webapp', [
-  angularMeteor,
-  webMain,
-  webSidebar,
-  webHeader,
-  webFooter
+    angularMeteor,
+    webMain,
+    webSidebar,
+    webHeader,
+    webFooter,
+    uiRouter,
+    WebAppServices
 ]);
 
-app.$inject = ["$rootScope"];
+app.$inject = ['$rootScope', 'WebAppConf'];
 
 app.config(configWebapp);
 app.run(runWebapp);
 
-function configWebapp(){
+function configWebapp($stateProvider, $locationProvider, $urlRouterProvider) {
     console.log('config() de webapp.......');
+    //Si es una vista no activa no funcionará un $state.go() si el config es el que define el 'state'
+    //por lo que hay que definirlos en el config del módulo principal
+    $stateProvider
+        .state('dashboard', {
+            url: '/dashboard',
+            template: '<view-user-dashboard></view-user-dashboard>',
+            params: {
+                user: { value: {} }
+            }
+        })
+        .state('agora', {
+            url: '/agoras/:agoraSlug',
+            template: '<view-agora></view-agora>'
+        })
+        .state('agoras', {
+            url: '/agoras',
+            template: '<view-agora-list></view-agora-list>'
+        });
 
-    Accounts.onLogin(function(){
-        console.log("onLogin() en cliente!!!");
-        var user = Meteor.user();
-        console.log(user);
-        if(user.profile.loggings === 0){
-            console.log("Es el primer logging, así que le redirijo a su dashboard");
-            //TODO: redirigir al dashboard de usuario.
-        }
-        //TODO: si refresco la página suma un login por que la session se mantiene en caché, es como si estaría contando conexiones.
-        //Quizás al refrescar haya que desconectar o solo contar cuando se obtiene un token de session.
-        user.profile.loggings += 1;
-        console.log(Meteor.userId());
-        Meteor.users.update({ "_id": Meteor.userId()}, { $set : {"profile": user.profile}});
-
-    });
-
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
 }
 
-function runWebapp($rootScope){
+function runWebapp($rootScope, WebAppConf){
     console.log("run() de webapp ......");
     $rootScope.userId = Meteor.userId();
+    WebAppConf.onLogin();
 
 }
