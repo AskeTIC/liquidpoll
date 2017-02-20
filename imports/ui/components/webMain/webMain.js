e1f72295f7588e3d1cbe1047cd83c57185c88bbd
jonabasque <jonmultimedia@gmail.com>
2017-02-20 02:22:32 +0100

    Completed before commit

--- imports/ui/components/webMain/webMain.js
+++ imports/ui/components/webMain/webMain.js
@@ -42,9 +42,4 @@ export default angular.module(name, [
     function run($rootScope, $state) {
         'ngInject';
         console.log('run() de webMain......');
-        $rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {
-          if (error === 'AUTH_REQUIRED') {
-            $state.go('app');
-          }
-    });
-}
+    }
