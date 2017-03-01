a7cd27ec655044f945a334516c5380f078c4e20a
jonabasque <jonmultimedia@gmail.com>
2017-03-01 07:20:16 +0100

    Extracted 'agora' state manage & commented tools

--- imports/ui/components/viewAgora/viewAgora.js
+++ imports/ui/components/viewAgora/viewAgora.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import {default as utils} from 'asketic-utils';
-//TODO: Recibir por get o post el objeto con el 'parli' en lugar de hacer otra consulta a la BBDD
+
 import { Agoras } from '../../../api/agoras/agoras'; //this cursors is going to work with monimongo
 
 //Componentes incluidos (dependencias)
@@ -19,7 +19,7 @@ class ViewAgora {
         $reactive(this).attach($scope);
         console.log("viewAgora controller!");
         var that = this;
-        //console.log($stateParams.agoraSlug);
+        console.log($stateParams.agoraSlug);
         //console.log($rootScope.userId);
 
         //TODO: Conseguir extraer el último voto del usuario con la consulta.
@@ -50,66 +50,7 @@ class ViewAgora {
                 ctx: '2d'
             }
         }
-        /*
-        this.change = function(){
-            Agoras.update(
-                { _id: that.agora._id},
-                { $set: { name: 'Argentina'} }
-            );
-        }
-        this.change2 = function(){
-            var newAgora = {
-                'name': 'Mendoza',
-                'slug': 'mendoza'
-            };
-            that.agora.subagoras.forEach(function(value, key){
-                delete value.$$hashKey;
-            });
-            that.agora.subagoras.push(newAgora);
-            Agoras.update(
-                { _id: that.agora._id},
-                { $set: { subagoras: that.agora.subagoras } }
-            );
-        }
-        this.change3 = function(){
-            var newAgora = {
-                "name" : "Portugal",
-                "slug" : "portugal",
-                "description" : "Ágora portuguesa.",
-                "entities" : [
-                  { "name" : "Cambiemos", "siglas" : "Cambiemos", "points" : 5, "percent" : null, "color" : "blue" },
-                  { "name" : "Frente para la victoria","siglas" : "FxV","points" : 2,"percent" : null, "color" : "pink" },
-                  { "name" : "Justicia social", "siglas" : "JS", "points" : 3, "percent" : null, "color" : "brown" },
-                  { "name" : "Todo por la plata", "siglas" : "TxP", "points" : 10, "percent" : null, "color" : "lightgreen" },
-                  { "name" : "Iluminatis argentinos", "siglas" : "Iluminatis", "points" : 12, "percent" : null, "color" : "yellow" },
-                  { "name" : "Partido maligno", "siglas" : "PM", "points" : 13, "percent" : null, "color" : "orange" },
-                  { "name" : "Agarralo como puedas", "siglas" : "Agarralo", "points" : 7, "percent" : null, "color" : "violet" },
-                  { "name" : "Los viejos verdes", "siglas" : "LV", "points" : 4, "percent" : null, "color" : "green" },
-                  { "name" : "Los jovenes con el pindar rojo", "siglas" : "PC", "points" : 4, "percent" : null, "color" : "red" },
-                  { "name" : "Los hombres de negro", "siglas" : "Sociatas", "points" : 1, "percent" : null, "color" : "lightblue" }
-                ],
-                "subagoras" : [
-                  { "name" : "Cordoba", "slug" : "cordoba" },
-                  { "name" : "Buenos Aires", "slug" : "buenos-aires" }
-                ],
-                "topagoras" : [ ],
-                "settings" : { "nodes" : [ 1 ], "area-level" : 4, "area-type" : 3 }
-            };
-            Agoras.insert(newAgora);
-        }
-        this.myVar = 10;
-        this.autorun(() => {
-            console.log('Autorun!!', this.getReactively('myVar'));
-            console.log('Lo que sea desde autorun()!!!!');
-        });
-        this.change4 = function(){
-            this.myVar +=1;
-        }
-        this.foo = 0;
-        this.change5 = function(){
-            this.foo +=1;
-        }
-        */
+
         //TOKNOW: Los helpers se ejecutan cunado se crea el controller, para poner datos o métodos en disposicion de la vista, sin embargo si
         //TOKNOW: Que diceferencia hay entre $scope o this con reactive y con agregarlos mediante esta function helpers() ???
         //TOKNOW: Estos helpers tienen vetadas algunas propiedades de configurable, writable o readable (parece que es de JS nativo).
@@ -163,13 +104,6 @@ export default angular.module(name, [
 	})
 	.config(config);
 
-function config($stateProvider) {
+function config() {
 	'ngInject';
-    $stateProvider
-        .state('agora', {
-            url: '/agoras/:agoraSlug',
-            template: '<view-agora></view-agora>'
-        });
-
-
 }
